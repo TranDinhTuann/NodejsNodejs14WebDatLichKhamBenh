95afd6c8ffa3d2c8307eb1107fb7c6f645f79ce3
TranDinhTuann <dtuan12122002@gmail.com>
2024-03-04 15:00:00 +0700

    37p14

--- src/services/userService.js
+++ src/services/userService.js
@@ -60,6 +60,28 @@ let checkUserEmail = (userEmail) => {
         }
     })
 }
+
+let getAllUsers = (userId) => {
+    return new Promise(async (resolve,reject) => {
+        try {
+            let users = '';
+            if(userId === 'ALL'){
+                users = await db.User.findAll({
+
+                })
+            }
+            if(userId && userId !== 'ALL'){
+                users = await db.User.findOne({
+                    where: {id : userId},
+                })
+            }
+            resolve(users)
+        } catch (e) {
+            reject(e);
+        }
+    } )    
+}
 module.exports = {
     handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers,
 }
